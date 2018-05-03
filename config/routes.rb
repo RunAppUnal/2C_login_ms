7d7b0cc5fe0d782c1c6f9f7c2c6c2d1e68280ecd
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-05-03 04:12:56 -0500

    findUser

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {registrations: "registrations", sessions: "sessions"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/userById/:id', to: 'user#findUserById'
+  get '/auth/userByUsername/:username', to: 'user#findUserByUsername'
+
 end
